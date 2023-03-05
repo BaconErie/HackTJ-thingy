743168661d1d7539af31833d487074700768dcbe
BaconErie <baconerie0001@gmail.com>
2023-03-04 21:29:23 -0500

    Do the chatgpt stuff

--- backend/is_blocked.js
+++ backend/is_blocked.js
@@ -1,23 +1,34 @@
-import { Configuration, OpenAIApi } from "openai";
-var request = require('sync-request');
+import {Configuration, OpenAIApi} from 'openai';
+
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 export default async function isABlockedCategory(categories, url) {
     let response = await fetch(url);
     const content = await response.text();
     
-    let regex = /<title>([\s\S]*?)<\/title>/g;
+    let regex = /(?<=<title>)(.*)(?=<\/title>)/g;
 
-    title = content.match(regex)[0];
+    let title = content.match(regex)[0];
 
-    console.log(title)
+    for (let category of categories) {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Is ${title} related to ${category}? Yes or No?`,
+            temperature: 0.0
+        })
+
+        let output = completion.data.choices[0].text 
+
+        if (output.includes('Yes')) {
+            return true;
+        }
+    }
+
+    return false;
 }
-
-/* TEST CODE
-const isABlockedCategory = require('./is_blocked').isABlockedCategory;
-isABlockedCategory('https://www.roblox.com');
-*/
+console.log(await isABlockedCategory(['game'], 'https://www.youtube.com/watch?v=TlB_eWDSMt4'));
